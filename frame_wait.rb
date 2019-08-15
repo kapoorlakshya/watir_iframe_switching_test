35563207b9f5c2d8aa76c41445de257a9b4862a3
Lakshya Kapoor <Lakshya.Kapoor@exlservice.com>
2019-08-15 16:43:33 -0700

    Fix how the hook was added.

--- frame_wait.rb
+++ frame_wait.rb
@@ -2,8 +2,8 @@ require 'webdrivers'
 require 'watir'
 
 @browser = Watir::Browser.new :chrome
-hook     = proc { puts "Heading found: #{@browser.h1(id: 'page_title').present?}" }
-@browser.after_hooks.add { hook }
+hook     = proc { @heading_found = @browser.h1(id: 'page_title').present? }
+@browser.after_hooks.add hook
 
 # Navigate to test page
 @browser.goto File.join('file://', File.expand_path('frame_test.html'))
@@ -12,6 +12,9 @@ hook     = proc { puts "Heading found: #{@browser.h1(id: 'page_title').present?}
 puts "IFrame found: #{@browser.iframe(id: 'watir').present?}"
 
 # Look for Div inside iframe
-puts @browser.iframe(id: 'watir').div(class: 'intro').text
+puts "Div text: #{@browser.iframe(id: 'watir').div(class: 'intro').text}"
+
+# Check for presence of heading element outside the iframe.
+puts "Heading found: #{@heading_found}"
 
 @browser.close
